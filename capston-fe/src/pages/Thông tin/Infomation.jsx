eb5b87def847a2c222e8f105785bcfa37453e6c0
tranminhnhut1999 <tranminhnhut852@gmail.com>
2023-10-15 20:40:43 +0700

    update 1

---
+++ capston-fe/src/pages/Thông tin/Infomation.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Infomation() {
+  return <div>Infomation</div>;
+}
---
+++ capston-fe/src/routes/Router.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+
+export default function Router() {
+  const routing = useRoutes([]);
+  return routing;
+}
